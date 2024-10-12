056dc3e8d3b6f5195fc21592cf69ac1e45a6019e
Vijay Palakuri <skgoud1434@gmail.com>
2024-10-12 19:22:48 +0530

    productCard updated

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import "./ProductCard.css"
-import {Card, CardContent,CardMedia} from "@mui/material";
-
+import React from 'react';
+import './ProductCard.css';
+import { Card, CardContent, CardMedia } from '@mui/material';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-export default function ProductCard(props) {
 
-    
+export default function ProductCard(props) {
   return (
     <Card className="card">
-    <CardMedia
-      component="img"
-      image={props.props.image}
-      alt={props.props.title}
-      height="400"
-    />
-    <CardContent className='content'>
-      <span className='title'>{props.props.title}</span>
-      <div className='signInContainer'>
-
-     
-      <span className='signIn'>Sign in or Create an account to see pricing</span>
-        <FavoriteBorderIcon/>
-      </div>
-    </CardContent>
+      <CardMedia
+        component="img"
+        image={props.props.image}
+        alt={props.props.title}
+        height="400"
+        loading="eager"  
+      />
+      <CardContent className="content">
+        <span className="title">{props.props.title}</span>
+        <div className="signInContainer">
+          <span className="signIn">
+            Sign in or Create an account to see pricing
+          </span>
+          <FavoriteBorderIcon />
+        </div>
+      </CardContent>
     </Card>
-  )
+  );
 }
